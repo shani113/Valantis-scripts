c7a34dc9cb41fa2d0b1c456701797259333571d3
shanishah711 <123447995+shanishah711@users.noreply.github.com>
2023-02-07 15:19:51 +0500

    Add files via upload

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -1,11 +1,8 @@
-import pytest
-from _pytest.fixtures import fixture
+
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.chrome import ChromeDriverManager
+
 import pytest
 @pytest.fixture()
 def setup():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver = webdriver.Chrome()
     return driver
